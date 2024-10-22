eead3a029913f0c4aaa77d233ee4af534a68b548
sbourzai <sbourzaim@gmail.com>
2024-10-22 21:59:42 +0200

    update sg & env-prefix

--- vpc.tf
+++ vpc.tf
@@ -40,12 +40,12 @@ resource "aws_default_security_group" "default-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  # ingress {
+  #   from_port   = 8080
+  #   to_port     = 8080
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
   egress {
     from_port   = 0
     to_port     = 0
