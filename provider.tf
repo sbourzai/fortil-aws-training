ed4e3437353474b8d4b2fe2530dbbecdb3d29d7c
sbourzai <sbourzaim@gmail.com>
2025-05-04 14:55:33 +0200

    update project

--- provider.tf
+++ provider.tf
@@ -3,7 +3,7 @@ terraform {
   backend "s3" {
     bucket = "aws-training-terraform-states"
     region = "eu-west-1"
-    key    = "load-balancer-root/terraform.tfstate"
+    key    = "load-balancer-root/terraform.tfstate" # to be changed
   }
   required_providers {
     aws = {
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,4 +1,4 @@
-vpc_cidr_block    = "10.0.0.0/23" # 512 IPs 
+vpc_cidr_block    = "10.0.0.0/23" # 512 IPs # to be changed
 availability_zone = "eu-west-1a"
-env_prefix        = "dev-root"
+env_prefix        = "dev-root" # to be changed
 instance_type     = "t2.micro"
